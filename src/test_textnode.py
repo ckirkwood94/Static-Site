8fdc0ff320bb76d8e8000aac7b9632241ef7bb56
ckirkwood94 <ckirkwood94@gmail.com>
2024-04-30 20:10:47 -0400

    Added functions to create links and images. Added tests for functions.

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,8 @@ from textnode import (
     text_type_image,
     text_type_link,
     split_nodes_delimiter,
+    extract_markdown_links,
+    extract_markdown_images,
 )
 
 class TestTextNode(unittest.TestCase):
@@ -106,6 +108,24 @@ class TestInlineMarkdown(unittest.TestCase):
                 TextNode(" word", text_type_text),
             ],
             new_nodes,
+        )
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                ("link", "https://boot.dev"),
+                ("another link", "https://blog.boot.dev"),
+            ],
+            matches,
         )
 
 if __name__ == "__main__":
